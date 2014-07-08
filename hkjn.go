3ec28d31eee0e8f0c02404dc3a571dc694f69c6d
Henrik Jonsson <me@hkjn.me>
2014-07-08 15:02:13 +0200

    adds link to autosite

--- hkjn.go
+++ hkjn.go
@@ -1,4 +1,6 @@
 // Package hkjnweb is the personal website http://www.hkjn.me / http://blog.hkjn.me.
+//
+// See https://github.com/hkjn/autosite for the framework that enables this site.
 package hkjnweb
 
 import (
