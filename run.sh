276abaed47e1874946b97b28882f80500d36c82b
Henrik Jonsson <me@hkjn.me>
2016-01-12 14:38:22 +0100

    Also kill any existing process before running

--- run.sh
+++ run.sh
@@ -10,4 +10,7 @@ export BIND_ADDR=:4430
 export PROD=1
 go get -u hkjn.me/hkjnweb/...
 go build ./cmd/server/hkjnserver.go
+if pgrep hkjnserver 1>/dev/null; then
+	kill $(pgrep hkjnserver)
+fi
 ./hkjnserver -alsologtostderr
