ceaa4a01972f511e18464f28705cc3746fdae0ab
Henrik Jonsson <me@hkjn.me>
2016-01-12 14:33:24 +0100

    Add run.sh
    This script builds and runs the hkjnweb server with production
    settings.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Runs the web server using production settings.
+#
+set -euo pipefail
+
+export HTTPS_CERT_FILE=/etc/ssl/private/hkjn.me.crt
+export HTTPS_KEY_FILE=/etc/ssl/private/hkjn.me.key
+export BIND_ADDR=:4430
+export PROD=1
+go build ./cmd/server/hkjnserver.go
+./hkjnserver -alsologtostderr
