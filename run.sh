11a69056a4fa9750030adde97324e493616c264d
Henrik Jonsson <me@hkjn.me>
2016-01-12 14:37:56 +0100

    Also pull down any updates before building and running

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ export HTTPS_CERT_FILE=/etc/ssl/private/hkjn.me.crt
 export HTTPS_KEY_FILE=/etc/ssl/private/hkjn.me.key
 export BIND_ADDR=:4430
 export PROD=1
+go get -u hkjn.me/hkjnweb/...
 go build ./cmd/server/hkjnserver.go
 ./hkjnserver -alsologtostderr
